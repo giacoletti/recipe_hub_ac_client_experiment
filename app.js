42d40b64c7f69e574818337e88b300072205c785
giacoletti <giacoletti92@gmail.com>
2022-02-14 17:24:40 +0100

    Set cors to true in serve script change action cable version in index.html refactor app.js

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-const acServer = "http://localhost:3000/cable";
+// const acServer = "http://localhost:3000/cable";
 
-const cable = ActionCable.createConsumer(acServer);
-cable.subscriptions.create(
-  { channel: "NotificationsChannel" },
-  {
-    connected() {
-      console.log("connected to WS server");
-    },
-    received(data) {
-      debugger;
-      console.log(data);
-    },
+// const cable = ActionCable.createConsumer(acServer);
+// cable.subscriptions.create(
+//   { channel: "NotificationsChannel" },
+//   {
+//     connected() {
+//       console.log("connected to WS server");
+//     },
+//     received(data) {
+//       debugger;
+//       console.log(data);
+//     },
 
-    disconnected() {
-      console.log("disconnected from WS server");
-    }
-  }
-);
+//     disconnected() {
+//       console.log("disconnected from WS server");
+//     }
+//   }
+// );
+
+(function () {
+  this.App || (this.App = {});
+  const acServer = "ws://localhost:3000/cable";
+  this.App.cable = ActionCable.createConsumer(acServer);
+}.call(this));
+
+App.room = App.cable.subscriptions.create({channel: "NotificationsChannel"}, {
+  connected() {
+    console.log("connected to WS server");
+  },
+  receive: function(data) {
+    console.log('got data from WS server');
+  },
+  disconnected() {
+    console.log("disconnected from WS server");
+  },
+});
