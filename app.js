f654382a96418bca15804f78d7c02e36b8cfe4df
giacoletti <giacoletti92@gmail.com>
2022-02-14 16:26:09 +0100

    Format code and update app.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const acServer = "http://localhost:3000/cable"
+const acServer = "http://localhost:3000/cable";
 
-cable = ActionCable.createConsumer(acServer)
-cable.subscriptions.create({channel: 'NotificationsChannel'}, {
-  connected() {
-    console.log('connected to WS server')
-  },
-  received(data) {
-    debugger
-    console.log(data)
-  },
+const cable = ActionCable.createConsumer(acServer);
+cable.subscriptions.create(
+  { channel: "NotificationsChannel" },
+  {
+    connected() {
+      console.log("connected to WS server");
+    },
+    received(data) {
+      debugger;
+      console.log(data);
+    },
 
-  disconnected() {
-    console.log('disconnected from WS server')
+    disconnected() {
+      console.log("disconnected from WS server");
+    }
   }
-})
+);
